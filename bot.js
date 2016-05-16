0e26753f44b38b41e00f0619b0f13122e1798bb0
tommypacker <tommypacker@gmail.com>
2016-05-16 15:07:04 -0700

    Added in error handling and tweaks for weather

--- bot.js
+++ bot.js
@@ -24,7 +24,10 @@ controller.hears(['hello', 'hi', 'hey'],'direct_message,direct_mention,mention,a
 });
 
 controller.hears(['jarvis', 'butler'],'direct_message,direct_mention,mention,ambient', function(bot, message){
-	bot.reply(message, 'What can I do for you ' + getUserName(message) + '?');
+	bot.api.users.list({exclude_archived: 1}, function (err, res) {
+  		messageUser = getUser(res.members, message.user);
+		bot.reply(message, 'What can I do for you ' + messageUser.profile.first_name + '?');
+	});
 });
 
 controller.on('channel_leave',function(bot,message) {
@@ -37,7 +40,6 @@ controller.on('user_channel_join',function(bot,message) {
 
 controller.hears(['weather in'],'direct_message,direct_mention,mention,ambient', function(bot, message){
 	var location = message.text.substring(message.text.indexOf('in')+3);
-	console.log(location);
 	weather.getWeather(location, function(err, returnMessage){
 		if(returnMessage){
 			bot.reply(message, returnMessage);
@@ -45,7 +47,7 @@ controller.hears(['weather in'],'direct_message,direct_mention,mention,ambient',
 	});
 });
 
-
+//Helper Functions
 function getUserName(message){
 	return "<@" + message.user + "|cal>";
 }
--- weather.js
+++ weather.js
@@ -2,6 +2,7 @@ var https = require('https');
 
 var getWeather = function (location, callback){
 	var url = 'https://query.yahooapis.com/v1/public/yql?q=select item from weather.forecast where woeid in (select woeid from geo.places where text=' + '\'' + location + '\'' + ')&format=json';
+	//console.log(url);
 
 	https.get(url, function(res){
 		res.setEncoding('binary');
@@ -12,15 +13,24 @@ var getWeather = function (location, callback){
 
 		res.on('end', function(){
 			var result = JSON.parse(resData);
-			if (Array.isArray(result.query.results.channel)) {
-                var returnMessage = 'It is currently ' + result.query.results.channel[0].item.condition.temp + ' degrees and ' + result.query.results.channel[0].item.condition.text;
+			if(result.query.results == null){
+				callback(null, "Sorry, that's an invalid location");
+			}
+			else if(Array.isArray(result.query.results.channel)) {
+				var locationString = result.query.results.channel[0].item.title;
+				//console.log(parseLocation(locationString));
+                var returnMessage = 'It\'s currently ' + result.query.results.channel[0].item.condition.temp + ' degrees and ' + result.query.results.channel[0].item.condition.text + ' in ' + parseLocation(locationString);
                 callback(null, returnMessage);
-            } else {
-                var returnMessage = 'It is currently ' + result.query.results.channel.item.condition.temp + ' degrees and ' + result.query.results.channel.item.condition.text;
+            }else{
+                var returnMessage = 'It\'s currently ' + result.query.results.channel.item.condition.temp + ' degrees and ' + result.query.results.channel.item.condition.text  + ' in ' + parseLocation(locationString);
                 callback(null, returnMessage);
             }
 		})
 	});
+}
+
+function parseLocation(string){
+	return string.substring(string.indexOf('for')+4, string.indexOf(','));
 }
 
 /*getWeather('Chicago', function (err, message) {
